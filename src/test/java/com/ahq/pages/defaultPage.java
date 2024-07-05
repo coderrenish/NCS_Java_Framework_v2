53aff3669141a0a5f71dfdcfec0bcfaf1fa1f0d8
coderrenish <coderrenish@users.noreply.github.com>
2024-07-05 13:27:58 +1000

    Adding Git Ignore

--- src/test/java/com/ahq/pages/defaultPage.java
+++ src/test/java/com/ahq/pages/defaultPage.java
@@ -2,10 +2,12 @@ package com.ahq.pages;
 
 import com.ahq.globals.BrowserGlobal;
 import com.qmetry.qaf.automation.step.QAFTestStep;
+import io.cucumber.java.en.And;
 
 
 public class defaultPage {
-    @QAFTestStep(description = "TEst Step here with {0}")
+    @QAFTestStep(description = "Test Step here with {0}")
+    @And("Test Step here with {string}")
     public void testStepHere(String test) throws Exception {
         BrowserGlobal.iComment(test);
     }
