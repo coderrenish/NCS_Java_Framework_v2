ef28d7ef8e9b8e7356a4d9cff51889548651558a
coderrenish <coderrenish@users.noreply.github.com>
2024-11-28 19:43:36 +1000

    Utils: Passport number generator for Singapore and Australia

--- src/test/java/com/ahq/pages/demoCommSteps.java
+++ src/test/java/com/ahq/pages/demoCommSteps.java
@@ -1,5 +1,6 @@
 package com.ahq.pages;
 
+import com.ahq.globals.BrowserGlobal;
 import com.ahq.globals.web;
 import com.qmetry.qaf.automation.step.QAFTestStep;
 import io.cucumber.java.en.And;
@@ -10,14 +11,26 @@ public class demoCommSteps {
     @And("I Verify {string} page header text is {string}")
     public void iVerifyPageHeaderText(String pageName, String headerText) throws Exception {
         web.setPageName_Web(pageName);
-        web.waitAndVerifyPageHeaderTextWithPageName_Web(headerText,pageName);
+        web.verifyPageHeader_Web(headerText,pageName);
     }
 
     @QAFTestStep(description = "I Verify {0} page title text is {1}")
     @And("I Verify {string} page title text is {string}")
     public void iVerifyPageTitleText(String pageName, String titleText) throws Exception {
         web.setPageName_Web(pageName);
-        web.waitAndVerifyPageTitleTextWithPageName_Web(titleText,pageName);
+        web.verifyPageTitle_Web(titleText,pageName);
+    }
+
+    @QAFTestStep(description = "I fail step")
+    @And("I fail step")
+    public void iFailStep() throws Exception {
+        BrowserGlobal.iFailStepWithInfo("");
+    }
+
+    @QAFTestStep(description = "I skip step")
+    @And("I skip step")
+    public void iSkipStep() throws Exception {
+//
     }
 
 }
